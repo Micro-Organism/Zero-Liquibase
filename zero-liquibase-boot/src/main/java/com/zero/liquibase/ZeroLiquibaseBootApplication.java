aac86289a56260e4529209230c4f066718952bd4
Micro-Organism <coolrabbit1993@gmail.com>
2024-11-08 08:49:04 +0800

    feat: springboot 集成 liquibase

---
+++ zero-liquibase-boot/src/main/java/com/zero/liquibase/ZeroLiquibaseBootApplication.java
@@ -0,0 +1,13 @@
+package com.zero.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroLiquibaseBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroLiquibaseBootApplication.class, args);
+    }
+
+}
---
+++ zero-liquibase-boot/src/test/java/com/zero/liquibase/ZeroLiquibaseBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroLiquibaseBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
